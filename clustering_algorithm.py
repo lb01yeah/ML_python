44d6e827d3edc287cf02b2b264d8d888b51c419b
lb01y <lb01yeah@yeah.net>
2018-07-15 22:12:18 +0800

    add聚类算法K-means 注释

--- clustering_algorithm.py
+++ clustering_algorithm.py
@@ -12,13 +12,16 @@ x = np.append(x, x1)
 y = np.append(y, y1)
 
 x = np.append(x, y)
+#重组数据1100行，2列
 X = x.reshape(1100, 2)
 
 plt.figure(figsize=(10, 4))
 plt.subplot(121)
 plt.scatter(X[:, 0], X[:, 1], s=5)
-
-y_pred = KMeans(n_clusters=2,random_state=170).fit_predict(X)
+print(X)
+print(X[:, 0])
+#Kmeans算法，n_clusters为簇，random_state随机生成簇中心的状态条件
+y_pred = KMeans(n_clusters=6, random_state=170).fit_predict(X)
 
 plt.subplot(122)
 plt.scatter(X[:, 0], X[:, 1], s=5, c=y_pred)
